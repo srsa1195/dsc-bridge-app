d9a6f184bc16e42db4b5bf85e43652b04d1a74ec
Sruthi Sampath Kumar <sruthi@Sruthis-MacBook-Pro.local>
2022-12-02 14:38:27 -0700

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { AccountBox } from "./components/accountBox";
+import MapBox from "./components/Map";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <AppContainer>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AccountBox />} />
+        <Route path="/map" element={<MapBox />} />
+      </Routes>
+    </Router>
+    </AppContainer>
+  </>
   );
 }
 
---
+++ src/components/accountBox/loginForm.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import {
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
+} from "./common";
+import { Marginer } from "../marginer";
+import { AccountContext } from "./accountContext";
+import { useNavigate } from "react-router-dom";
+export function LoginForm(props) {
+  const navigate = useNavigate();
+  const { switchToSignup } = useContext(AccountContext);
+
+
+  return (
+    <BoxContainer>
+      <FormContainer>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+      </FormContainer>
+      <Marginer direction="vertical" margin={10} />
+      <MutedLink href="#">Forget your password?</MutedLink>
+      <Marginer direction="vertical" margin="1.6em" />
+      <SubmitButton type="submit" onClick={() => navigate("/map")}>Sign In</SubmitButton>
+      <Marginer direction="vertical" margin="1em" />
+      <MutedLink href="#">
+        Don't have an accoun?{" "}
+        <BoldLink href="#" onClick={switchToSignup}>
+          Signup
+        </BoldLink>
+      </MutedLink>
+    </BoxContainer>
+  );
+}
