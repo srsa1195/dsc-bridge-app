f2bf3e55b1aecd728cef643f52f46d7b1579335c
harshakalmath <50831461+harshakalmath@users.noreply.github.com>
2022-12-08 18:25:03 -0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,18 @@
 #native american , asian , black , white , hispanic , pacific islander
 #reading , hiking , workout , cooking , poetry , dancing , board games, foodie
 import base64
+import bson.json_util
 from flask import Flask, request, Response
-from Getdatabase import check_creds , insert_person_data , add_creds , get_person_data , add_filters , add_gender_interest
+from bson.json_util import loads, dumps
+from Getdatabase import check_creds , insert_person_data , add_creds , get_person_data , add_filters , add_gender_interest , add_ethnicity_interest
 import jsonpickle, pickle
 import platform
 import io, os, sys
+import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app=app)
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -24,55 +29,44 @@ def add_person_data():
 
 @app.route('/api/v1/setFilter/', methods=['POST'])
 def set_filter():
-    person_id = request.json['_id']
+    person_id = request.json['_id']['$oid']
     filters = request.json['filters']
     personObj = add_filters(person_id,filters)
-    response = {
-            "person" : personObj
-        }
-    return Response(response=response, status=200, mimetype="application/json")
+    return Response(response=personObj, status=200, mimetype="application/json")
 
 
 @app.route('/api/v1/setGenderInterest/', methods=['POST'])
 def set_genderInterest():
-    person_id = request.json['_id']
+    person_id = request.json['_id']['$oid']
     genderInterest = request.json['gender_interest']
     personObj = add_gender_interest(person_id,genderInterest)
-    response = {
-            "person" : personObj
-        }
-    return Response(response=response, status=200, mimetype="application/json")
+    return Response(response=personObj, status=200, mimetype="application/json")
 
 
 @app.route('/api/v1/setEthnicityInterest/', methods=['POST'])
 def set_ethnicityInterest():
-    person_id = request.json['_id']
+    person_id = request.json['_id']['$oid']
     filters = request.json['ethnicity_interest']
-    personObj = add_filters(person_id,filters)
-    response = {
-            "person" : personObj
-        }
-    return Response(response=response, status=200, mimetype="application/json")
+    personObj = add_ethnicity_interest(person_id,filters)
+    return Response(response=personObj, status=200, mimetype="application/json")
 
 @app.route('/api/v1/login/', methods=['POST'])
 def login():
     username = request.json['email']
     password = request.json['password']
-    creds_list = [username,password]
+    creds_list = {"email":username,"password":password}
     success = check_creds(creds_list)
     person = get_person_data(creds_list)
     if(success==True):
-        response = {
-            "person" : person
-        }
-        return Response(response=response, status=200, mimetype="application/json")
+        return Response(response=person, status=200, mimetype="application/json")
     else:
         return Response(response="User not found!", status=404, mimetype="application/json")
-        #return response as error
+        
 
-#@app.route('/api/v1/login/', methods=['POST']) 
+#@app.route('/api/v1/getSimilarPeople/', methods=['POST']) 
 #def getSimilarPeople():
     #fetch similar interest people from database and then return the set of people as response
         
 
 app.run(host="0.0.0.0", port=5001)
+
