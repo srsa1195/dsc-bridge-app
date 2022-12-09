b96e01acf6bb53be7e2de4dc1e45538a78008ba2
harshakalmath <50831461+harshakalmath@users.noreply.github.com>
2022-12-08 18:25:47 -0700

    Update Getdatabase.py

--- backend/Getdatabase.py
+++ backend/Getdatabase.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import bson.json_util
+from bson.objectid import ObjectId
 from Database import get_database
 dbname = get_database()
 
 
 def insert_person_data(person_data):
+    if(person_data['gender']==['both']):
+        person_data['gender']=['female','male']
     collection_name = dbname["people_database"]
     person_1 = {
     "Name" : person_data['name'],
@@ -20,31 +24,38 @@ def insert_person_data(person_data):
     }
     collection_name.insert_one(person_1)
 
+def returnObjectId(id):
+    return ObjectId(id)
 
 def get_person_data(person_details):
     collection_name = dbname["people_database"]
+    print("Query email: ")
+    print(person_details['email'])
     myquery = {"Email id" : person_details['email']}
     mydoc = collection_name.find(myquery)
-    return mydoc
+    print(mydoc[0])
+    obj = bson.json_util.dumps(mydoc[0])
+    return obj
 
 
 def add_ethnicity_interest(person_id,ethnicity):
     collection_name = dbname["people_database"]
-    collection_name.update_one({'_id':person_id}, {"$set": { "Interested_Ethnicity": ethnicity }}, upsert=False)
+    collection_name.update_one({'_id':returnObjectId(person_id)}, {"$set": { "Interested_Ethnicity": ethnicity }}, upsert=False)
     return get_person_by_id(person_id)
 
 
 def add_gender_interest(person_id,gender_interest):
     collection_name = dbname["people_database"]
-    collection_name.update_one({'_id':person_id}, {"$set": { "Gender_Interest": gender_interest }}, upsert=False)
+    collection_name.update_one({'_id':returnObjectId(person_id)}, {"$set": { "Gender_Interest": gender_interest }}, upsert=False)
     return get_person_by_id(person_id)
 
 
 def  get_person_by_id(person_id):
     collection_name = dbname["people_database"]
-    myquery = {"_id" : person_id}
+    myquery = {"_id" : returnObjectId(person_id)}
     mydoc = collection_name.find(myquery)
-    return mydoc
+    obj = bson.json_util.dumps(mydoc[0])
+    return obj
 
 
 def add_image(imageUrl,person_data):
@@ -56,12 +67,12 @@ def add_image(imageUrl,person_data):
 
 def add_filters(person_id,filters):
     collection_name = dbname["people_database"]
-    collection_name.update_one({'_id':person_id}, {"$set": { "Interests": filters }}, upsert=False)
+    collection_name.update_one({'_id':returnObjectId(person_id)}, {"$set": { "Interests": filters }}, upsert=False)
     return get_person_by_id(person_id)
 
 
 def check_creds(person_data):
-    myquery = {"email" : person_data[0], "password" : person_data[1]}
+    myquery = {"email" : person_data['email'], "password" : person_data['password']}
     collection_name = dbname['login_database']
     mydoc = collection_name.find(myquery)
     if (pd.isnull(mydoc)):
@@ -75,8 +86,8 @@ def add_creds(person_data):
     print(person_data['email'])
     print(person_data['password'])
     creds = {
-        "Username" : person_data['email'],
-        "Password" : person_data['password']
+        "email" : person_data['email'],
+        "password" : person_data['password']
     }
     collection_name.insert_one(creds)
 
@@ -86,8 +97,8 @@ def find_similar_data(params):
     ethnicity_interest = params['ethnicity']
     interests = params['interests']
     collection_name = dbname["people_database"]
-    similar_people = collection_name.find({"Gender_Interest":gender_interest,"Interested_Ethnicity":ethnicity_interest,
-    "Interests":interests})
+    similar_people = collection_name.find({"Gender":gender_interest})
+    #will return json object array
     return similar_people
 
 
@@ -97,5 +108,14 @@ if __name__ == "__main__":
     #,'gender_interest':'male','age':23,'username':'hkalmath','password':'123456'}
     #insert_person_data(person_data=person_data)
     #add_creds(person_data=person_data)
-    params = {"gender":"female","ethnicity":{"asian","white"},"interests":{"cooking","dancing"}}
-    find_similar_data(params)
+    #params = {"gender":"female","ethnicity":["asian","white"],"interests":["cooking","dancing"]}
+    #docs = find_similar_data(params)
+    #print(list(docs))
+    #for doc in docs:
+    #    print(doc)
+    get_person_data({"email":"abc@gmail.com","password":"abc@123"})
+    #get_person_by_id()
+
+
+
+
