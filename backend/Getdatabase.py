1ba4431e8a7ce1df36e7870b5282d8c398e75335
harshakalmath <50831461+harshakalmath@users.noreply.github.com>
2022-12-08 21:50:09 -0700

    Update Getdatabase.py

--- backend/Getdatabase.py
+++ backend/Getdatabase.py
@@ -6,14 +6,14 @@ dbname = get_database()
 
 
 def insert_person_data(person_data):
-    if(person_data['gender']==['both']):
-        person_data['gender']=['female','male']
+    if(person_data['Gender_Interest']==['both']):
+        person_data['Gender_Interest']=['female','male']
     collection_name = dbname["people_database"]
     person_1 = {
     "Name" : person_data['Name'],
     "Address" :  person_data['Address'],
     "Location" : person_data['Location'],
-    "Email_id" : person_data['Email_id'],
+    "email" : person_data['email'],
     "Ethnicity" : person_data['Ethnicity'] ,
     "Interested_Ethnicity" : person_data['Interested_Ethnicity'],
     "Interests" : person_data['Interests'] ,
@@ -30,8 +30,8 @@ def returnObjectId(id):
 def get_person_data(person_details):
     collection_name = dbname["people_database"]
     print("Query email: ")
-    print(person_details['Email_id'])
-    myquery = {"Email_id" : person_details['Email_id']}
+    print(person_details['email'])
+    myquery = {"email" : person_details['email']}
     mydoc = collection_name.find(myquery)
     print(mydoc[0])
     obj = bson.json_util.dumps(mydoc[0])
@@ -46,6 +46,8 @@ def add_ethnicity_interest(person_id,ethnicity):
 
 def add_gender_interest(person_id,gender_interest):
     collection_name = dbname["people_database"]
+    if(gender_interest==['both']):
+        gender_interest = ['female','male']
     collection_name.update_one({'_id':returnObjectId(person_id)}, {"$set": { "Gender_Interest": gender_interest }}, upsert=False)
     return get_person_by_id(person_id)
 
@@ -60,7 +62,7 @@ def  get_person_by_id(person_id):
 
 def add_image(imageUrl,person_data):
     collection_name = dbname["people_database"]
-    myquery = {"Email_id" : person_data['Email_id']}
+    myquery = {"email" : person_data['email']}
     newvalue = { "$set": { "image_url": imageUrl } }
     collection_name.update_one(myquery,newvalue)
 
@@ -72,7 +74,7 @@ def add_filters(person_id,filters):
 
 
 def check_creds(person_data):
-    myquery = {"Email_id" : person_data['Email_id'], "password" : person_data['password']}
+    myquery = {"email" : person_data['email'], "password" : person_data['password']}
     collection_name = dbname['login_database']
     mydoc = collection_name.find(myquery)
     if (pd.isnull(mydoc)):
@@ -83,10 +85,10 @@ def check_creds(person_data):
 
 def add_creds(person_data):
     collection_name = dbname["login_database"]
-    print(person_data['Email_id'])
+    print(person_data['email'])
     print(person_data['password'])
     creds = {
-        "Email_id" : person_data['Email_id'],
+        "email" : person_data['email'],
         "password" : person_data['password']
     }
     collection_name.insert_one(creds)
